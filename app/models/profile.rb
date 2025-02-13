69759d42dec4fd9b60169ca2f9c9115618d26fa9
Gregoire Laqueille <gregoire@bulle-ikcs.com>
2025-02-13 21:42:02 +0400

    profiles + db skills educations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_many :educations
+  has_many :skills
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
+
+
+    # Routes pour les freelances (seuls leurs profils sont visibles)
+    resources :profiles, only: [:index, :show, :edit, :update, :destroy] do
+    end
+
+    # Routes pour les projets (créés par les entreprises, visibles par les freelances)
+    resources :projects, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+    # Routes pour les messages (messagerie entre freelances et entreprises)
+    resources :discussions, only: [:index, :show, :create] do
+      resources :messages, only: [:create]
+    end
+
+    # Route pour la gestion des entreprises (elles ne sont pas listées)
+    resources :users, only: [] do
+      resources :projects, only: [:index] # Pour qu'une entreprise voie ses propres projets
+    end
+  end
