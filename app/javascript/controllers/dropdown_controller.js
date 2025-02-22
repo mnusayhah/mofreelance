76d94c65c362ad22752bbd2731c63ab6589cdf7e
Nusayhah Mungur <nusayhahnm@gmail.com>
2025-02-22 13:53:47 +0400

    fixed dropdown menu and added search query in index

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,10 +1,1 @@
 import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  static targets = ["menu"];
-
-  toggle(event) {
-    event.preventDefault(); // Prevents unwanted link behavior
-    this.menuTarget.classList.toggle("hidden");
-  }
-}
